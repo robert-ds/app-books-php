12dd64fb632ea6be448f9581f3e122d6e00fa714
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-05-25 18:11:11 -0400

    credentials db

--- admin/config/db.php
+++ admin/config/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host       = "";
-$db         = "";
-$user       = "robert";
-$password   = "";
+$host       = "mysql-m";
+$db         = "m3517544_libros";
+$user       = "m3517544rw";
+$password   = "chocolate";
 
 try {
   $connect = new PDO("mysql:host=$host;dbname=$db",$user,$password);
